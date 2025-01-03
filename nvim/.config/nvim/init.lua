cd30653e13b2eb0f3f45f64741131d19c017e7e9
rx3lixir <anxi3ty.exe@proton.me>
2025-01-03 11:42:08 +0700

    arch0103

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,23 @@
+-- Setting up LazyVim options
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+vim.o.background = "light" -- or "light" for light mode
+
+-- Setting up vim options
+require("vim-options")
+
+-- Setting up LazyVim plugin manager
+require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } })
+
+-- Setting up russian keys in vim
+require("rusKey")
