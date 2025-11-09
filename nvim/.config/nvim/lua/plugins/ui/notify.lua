5349f2389ce6944bcdd03da2ecc823563c8eda73
rx3lixir <i027356@inbox.ru>
2025-11-09 22:23:34 +0700

    added nvim notifications

--- nvim/.config/nvim/lua/plugins/ui/notify.lua
+++ nvim/.config/nvim/lua/plugins/ui/notify.lua
@@ -1,4 +1,26 @@
 return {
 	"rcarriga/nvim-notify",
 	lazy = false,
+	config = function()
+		local notify = require("notify")
+
+		vim.notify = notify
+
+		-- Optional: configure how it looks and behaves
+		notify.setup({
+			-- Animation style (fade_in_slide_out, fade, slide, static)
+			stages = "fade_in_slide_out",
+
+			-- How long notifications stay on screen (in ms)
+			timeout = 2000,
+
+			-- Max width of notification window
+			max_width = 30,
+
+			-- Where notifications appear
+			-- top_left, top_right, bottom_left, bottom_right
+			-- or just top, bottom, left, right
+			render = "default",
+		})
+	end,
 }
